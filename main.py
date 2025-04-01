021c4b3d2d1347c81616d504635e8db80d8a5adf
horooga <saintnurlan@gmail.com>
2025-04-02 00:23:07 +0300

    need to redisign, auth, top

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, Form
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import yaml
+import time
 
+with open("questions.yaml", "r") as f:
+    answers = yaml.safe_load(f)
+    questions = [i for i in answers]
 app = FastAPI()
 templates = Jinja2Templates(directory="static")
 app.mount("/static", StaticFiles(directory="static"))
-questions = ["one plus two = ?", "color of a cow's milk is - ?"]
 
 @app.get("/")
-async def start(request: Request, response_class = HTMLResponse):
-    return templates.TemplateResponse("start.html", {"request": request})
+async def start(response_class = HTMLResponse):
+    return FileResponse("static/start.html")
 
 @app.post("/type")
-async def type(request: Request, response_class = HTMLResponse):
-    return templates.TemplateResponse("type.html", {"request": request, "question": questions[0]})
+async def type(request: Request, question: str = Form(default = None), answer: str = Form(default = None), start_time: str = Form(default = None), response_class = HTMLResponse):
+    if start_time:
+        return templates.TemplateResponse("type.html", {
+            "request": request,
+            "question": questions[1],
+            "res": "Awesome!" if answer == answers[question] else "Oh no!",
+            "time_elapsed": str(round(time.time() - float(start_time), 3)),
+            "start_time": str(round(time.time()))
+        })
+    else:
+        return templates.TemplateResponse("first_type.html", {
+            "request": request,
+            "question": questions[1],
+            "start_time": str(round(time.time()))
+        })
+    
